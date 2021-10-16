642c83f337e45e8193c385619a11d91ab021e8f7
Thor Correia <thor.lucas@berkeley.edu>
2021-10-16 16:00:30 -0700

    initial setup

---
+++ src/components/App/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const App: React.FC = () => {
+	return (
+	<div className="w-100 h-screen flex justify-center items-center">
+		<div className="px-12 py-8 bg-white rounded-lg shadow-md">
+			<h1 className="mb-2 text-3xl font-semibold">
+				Hello, world!
+			</h1>
+			<div>
+				<p>Welcome to my React Application!</p>
+			</div>
+		</div>
+	</div>
+	);
+}
+
+export default App;
