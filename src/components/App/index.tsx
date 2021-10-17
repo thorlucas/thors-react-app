96a12277f0427fdb1b2a971a8c6740687ce002b7
Thor Correia <thor.lucas@berkeley.edu>
2021-10-16 19:23:36 -0700

    used example adapter hook

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -3,12 +3,18 @@ import React from 'react';
 import Button from '@UI/Button';
 import { MyContext } from '@contexts';
 import { useWait } from '@hooks';
+import { Document } from '@adapters/document';
+import useDocument from '@hooks/document';
 
 const App: React.FC = () => {
 	// This is an example of the usage of a custom hook. We can extract logic this way.
 	// This value will be null initially, then become "Hello again, world!" after 2
 	// seconds.
 	const wait: string | null = useWait(2000, null, "Hello again, world!");
+
+	// This is a hook that calls an adapter which then fetches a document for us using
+	// an API. The document will be made available after one second.
+	const document: Document | null = useDocument('dummy');
 
 	return (
 	// This is a context provider. All subsequent components are able to use this
@@ -40,6 +46,13 @@ const App: React.FC = () => {
 					) : null }
 
 				</div>
+
+				{ document ? (
+					<div className="my-4 py-4 px-3 bg-gray-100 rounded-md shadow-md">
+						<h2 className="mb-2 text-2xl font-semibold">{ document.title }</h2>
+						<p>{ document.body }</p>
+					</div>
+				) : null }
 
 				<div>
 					{ /* This is an example button component */ }
