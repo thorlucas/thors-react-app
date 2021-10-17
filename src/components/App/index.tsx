f310643425dc87a05cb6772e531c2f07b1008460
Thor Correia <thor.lucas@berkeley.edu>
2021-10-16 18:20:58 -0700

    use example useWait hook

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,7 +1,11 @@
+import { useWait } from '@hooks';
 import { Button } from '@UI/Button';
 import React from 'react';
 
 const App: React.FC = () => {
+	// This is an example of the usage of a custom hook. We can extract logic this way.
+	const wait: string = useWait(2000, 'Waiting 2 seconds...', 'Hello world!');
+
 	return (
 	<div className="w-100 h-screen flex justify-center items-center">
 		<div className="px-12 py-8 bg-white rounded-lg shadow-md">
@@ -10,6 +14,7 @@ const App: React.FC = () => {
 			</h1>
 			<div className="mb-4">
 				<p>Welcome to my React Application!</p>
+				<p>{ wait }</p>
 			</div>
 			<div>
 				<Button role='primary'>
