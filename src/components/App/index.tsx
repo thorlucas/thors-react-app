c41f256777808faaf51536624d4cb805dc18433f
Thor Correia <thor.lucas@berkeley.edu>
2021-10-16 18:54:29 -0700

    added comments on App component

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -5,33 +5,51 @@ import React from 'react';
 
 const App: React.FC = () => {
 	// This is an example of the usage of a custom hook. We can extract logic this way.
-	const wait: string | null = useWait(2000, null, 'Hello again, world!');
+	// This value will be null initially, then become "Hello again, world!" after 2
+	// seconds.
+	const wait: string | null = useWait(2000, null, "Hello again, world!");
 
 	return (
+	// This is a context provider. All subsequent components are able to use this
+	// context value by either using a <MyContext.Consumer> element, or by using a
+	// useContext hook.
 	<MyContext.Provider value="Hello, world!">
+
 		<div className="w-100 h-screen flex justify-center items-center">
 			<div className="px-12 py-8 bg-white rounded-lg shadow-md">
+
 				<h1 className="mb-2 text-3xl font-semibold">
 					Hello, world!
 				</h1>
+
 				<div className="mb-4">
+
 					<p>Welcome to my React Application!</p>
+					
+					{ /* Here we are using a Consumer to use the context value. */ }
 					<MyContext.Consumer>
 						{ value => (
 							<p>{ value }</p>
 						)}
 					</MyContext.Consumer>
+
+					{ /* Here we are conditionally rendering only if wait is not null. */ }
 					{ wait ? (
 						<p>{ wait }</p>
 					) : null }
+
 				</div>
+
 				<div>
+					{ /* This is an example button component */ }
 					<Button role='primary'>
 						Hello!
 					</Button>
 				</div>
+
 			</div>
 		</div>
+
 	</MyContext.Provider>
 	);
 }
