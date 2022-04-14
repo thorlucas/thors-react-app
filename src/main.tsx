0c9f344e078fc05c8b4185c98732e9581369095b
Thor Correia <thor.lucas@icloud.com>
2022-04-14 03:02:47 -0700

    typescript

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+const App = () => {
+	return (
+		<>
+			Hi!
+		</>
+	);
+}
+
 ReactDOM.createRoot(
 	document.getElementById('root')!
 ).render(
 	<React.StrictMode>
+		<App />
 	</React.StrictMode>
 );
